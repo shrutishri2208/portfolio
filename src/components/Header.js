d4fcd6ecf4be15341e10ba6e44a4ffc1580b4dfa
Shruti Shrivastava <shrutishri2208@gmail.com>
2023-02-28 01:40:16 +0530

    resume

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,20 @@ import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
 
 const Header = () => {
+  const handleResumeClick = () => {
+    fetch("shrutiResume.pdf").then((response) => {
+      response.blob().then((blob) => {
+        // Creating new object of PDF file
+        const fileURL = window.URL.createObjectURL(blob);
+        // Setting various property values
+        let alink = document.createElement("a");
+        alink.href = fileURL;
+        alink.download = "shrutiResume.pdf";
+        alink.click();
+      });
+    });
+  };
+
   return (
     <div className="flex justify-between">
       <Link to="/">
@@ -14,7 +28,10 @@ const Header = () => {
         {/* <Link to="/projects">
           <h2 className="text-2xl ml-20">projects</h2>
         </Link> */}
-        <h2 className="2xl:text-xl xl:text-xl lg:scale-100 md:scale-95 sm:-mt-4 sm:scale-75 -mt-3 scale-75 mr-4 highlight1">
+        <h2
+          className="2xl:text-xl xl:text-xl lg:scale-100 md:scale-95 sm:-mt-4 sm:scale-75 -mt-3 scale-75 mr-4 highlight1"
+          onClick={handleResumeClick}
+        >
           resume
         </h2>
       </div>
